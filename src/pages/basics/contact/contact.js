0fa7cc59c53172351e3ed2fafa9d085185865ad7
liujie <626796235@qq.com>
2023-06-20 15:14:07 +0800

    更新后端地址

--- src/pages/basics/contact/contact.js
+++ src/pages/basics/contact/contact.js
@@ -4,14 +4,14 @@
  * @Authored: LiuJie
  * @Date: 2022-04-27 12:54:43
  * @LastEditors: LiuJie 626796235@qq.com
- * @LastEditTime: 2023-06-20 11:50:50
+ * @LastEditTime: 2023-06-20 14:50:24
  */
 import React, { useState } from "react"
 import Layout from "../../../components/layout"
 import * as contactStyles from "./contact.module.css"
 
 // const ipAddress = '47.97.71.176';
-const ipAddress = 'localhost';
+const ipAddress = '127.0.0.1';
 
 /** 接口定义 */
 const getJSONData = async () => {
